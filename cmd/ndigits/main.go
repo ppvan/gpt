5c5b018128e7d50b8cc82268977f8dbad9ba40b3
ppvan <phuclaplace@gmail.com>
2026-06-23 22:30:14 +0700

    refactor: delete things to focus on nn

--- cmd/ndigits/main.go
+++ cmd/ndigits/main.go
@@ -1,21 +1,51 @@
 package main
 
-//go:generate go-winres make --product-version=git-tag
-
 import (
-	"runtime"
+	"fmt"
 
-	"github.com/rodrigocfd/windigo/co"
-	"github.com/rodrigocfd/windigo/win"
+	"github.com/ppvan/gpt/nn"
 )
 
-const title = "Digit Recognizer Training"
-
 func main() {
-	runtime.LockOSThread() // important: Windows GUI must run on a single OS thread
+	xor()
+	// nand()
+}
 
-	win.CoInitializeEx(co.COINIT_APARTMENTTHREADED | co.COINIT_DISABLE_OLE1DDE)
-	defer win.CoUninitialize()
+func xor() {
+	x := nn.NewMat([][]float64{
+		{0, 0}, {0, 1}, {1, 0}, {1, 1},
+	})
+	labels := nn.NewMat([][]float64{
+		{0},
+		{1},
+		{1},
+		{0},
+	})
+	y := labels.OneHot(2)
 
-	ShowMainWindow()
+	model := nn.NewSequential(
+		nn.NewLinear(2, 4),
+		nn.Sigmoid(),
+		nn.NewLinear(4, 4),
+		nn.Sigmoid(),
+		nn.NewLinear(4, 2),
+	)
+
+	net := nn.NewNetwork(model, nn.CrossEntropy())
+
+	net.Train(100000, x, y, func(epoch int, loss float64) {
+		fmt.Printf("epoch %d: loss=%.6f\r", epoch, loss)
+	})
+
+	fmt.Println()
+
+	fmt.Println("===== FINAL PREDICTIONS =====")
+	for i := 0; i < x.Rows; i++ {
+		row := x.RowAt(i)
+		input := nn.NewRowMat(row)
+
+		logits := net.Infer(input)
+		class := logits.ArgMax().Get(0, 0)
+		fmt.Printf("%v | %v = %v\n", row[0], row[1], class)
+	}
 }
